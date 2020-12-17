e382742ab66949ce28bc9ad07f26178fb8e121ad
iximy <prcr98@gmail.com>
2020-12-17 08:14:20 +0300

    ver1

---
+++ 2804serverbuildv1.sh
@@ -0,0 +1,34 @@
+opkg update
+
+opkg install kmod-usb-core 
+opkg install kmod-usb2               
+opkg install kmod-usb-ohci
+opkg install kmod-usb-storage
+opkg install kmod-fs-ext4
+opkg install e2fsprogs
+opkg install mount-utils
+opkg install cfdisk
+opkg install f2fs-tools
+opkg install libf2fs
+opkg install block-mount
+opkg install kmod-fs-f2fs
+mkfs.ext4 /dev/sda1 
+block detect > /etc/config/fstab
+# reboot смонтировать и включить диск как корневой в luci 
+mkdir -p /tmp/introot
+mkdir -p /tmp/extroot
+mount --bind / /tmp/introot
+mount /dev/sda1 /tmp/extroot
+tar -C /tmp/introot -cvf - . | tar -C /tmp/extroot -xf -
+umount /tmp/introot
+umount /tmp/extroot
+
+opkg update
+opkg install nginx-all-module		
+opkg install luci-nginx
+
+
+
+
+
+
